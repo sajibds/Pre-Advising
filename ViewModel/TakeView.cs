421ca0d58b04b8ee1729c3c379ea46b773852f95
sajibds <36394850+sajibds@users.noreply.github.com>
2018-12-05 14:16:50 +0600

    Add files via upload

---
+++ ViewModel/TakeView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using project.Models;
+
+namespace project.ViewModel
+{
+    public class TakeView
+    {
+        public IEnumerable<course> Courses { get; set; }
+        public taken_courses Taken_Courses { get; set; }
+        public IEnumerable<Student> students { get; set; }
+    }
+}
